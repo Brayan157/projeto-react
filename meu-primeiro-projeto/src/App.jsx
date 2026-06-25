83bdbcc260f3374f097213caa16390eb833e097f
Gustavo Brayan <gustavo.brayan159@gmail.com>
2026-06-25 16:28:42 -0300

    frist commit

---
+++ meu-primeiro-projeto/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+        <h1> HELLO REACT</h1> 
+        <p className='caption'> Aprendendo React passo a passo</p> 
+        
+      </div>   
+    </>
+  )
+}
+
+export default App
